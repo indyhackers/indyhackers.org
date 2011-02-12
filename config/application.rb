5609521b24062c7e46f46e1d99a7ee424bf83475
Miles Z. Sterrett <miles.sterrett@gmail.com>
2011-02-12 18:04:22 -0500

    Template generator = HAML

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 module IndyhackersRails
   class Application < Rails::Application
     config.generators do |g|
+      g.template_engine :haml
       g.test_framework :rspec, :fixture => true, :views => false
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
