6f470dc3b4e311a18b0d96622a0bc32afcfba437
Miles Z. Sterrett <miles.sterrett@gmail.com>
2011-03-01 23:36:54 -0500

    Send me some job_post_requests and stuff

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -2,12 +2,16 @@ class SystemMailer < ActionMailer::Base
   default :from => "alfred@indyhackers.org"
   default :to => 'miles@indyhackers.org'
 
-  def feedback(job_post_request)
+  def job_post_request(job_post_request)
     @job_post_request = job_post_request
     if job_post_request.attachment.present?
       attachments[job_post_request.attachment.original_filename] = File.read(job_post_request.attachment.tempfile.path)
     end
-    mail(:subject => "[#{@job_post_request.issue_type}]")
+    mail(
+      :subject => "[#{@job_post_request.title}]", 
+      :from => %{"#{@job_post_request.name}" <alfred@indyhackers.org>},
+      :reply_to => @job_post_request.email
+    )
   end
 
 end
