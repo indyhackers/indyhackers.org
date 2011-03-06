1f6ace97c9c1f1bc4dab0221a2a03fac9cf2d587
Miles Z. Sterrett <miles.sterrett@gmail.com>
2011-03-05 23:04:00 -0500

    Order jobs by their creation date

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,4 +2,8 @@ class Admin::JobsController < InheritedResources::Base
   respond_to :html
 
   before_filter :authenticate_admin!
+
+  def collection
+    @jobs = Job.order("created_at DESC").all
+  end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,8 @@ class JobsController < InheritedResources::Base
   respond_to :atom, :only => :index
   respond_to :xml, :only => :index
   actions :index, :show
+
+  def collection
+    @jobs = Job.order("created_at DESC").all
+  end
 end
