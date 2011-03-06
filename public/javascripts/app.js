3b75224d513f5854994b3073a107a745f68373b0
Miles Z. Sterrett <miles.sterrett@gmail.com>
2011-03-05 23:21:52 -0500

    Only scroll to the job if we're opening it.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,16 +3,20 @@ $(document).ready(function() {
     $('a[name=' + location.hash.replace(/#/, '') + ']').parent().next().slideDown('slow');
   }
   $('.job h3 a').click(function(event) {
-    var anchorName = $(this).attr('name');
+    var el = $(this);
+    var anchorName = el.attr('name');
     event.stopImmediatePropagation();
     event.preventDefault();
-    $(this).parent().next().slideToggle('slow');
+    el.parent().next().slideToggle('slow', function() {
+      if ($(this).is(':visible')) {
+        // Animating the move or scroll to the element is better, I think.
+        $('html,body').animate({scrollTop: el.offset().top},'slow');
+      }
+    });
     // Remove the name so that setting the hash doesn't jump the page
-    $(this).attr('name', '');
+    el.attr('name', '');
     location.hash = anchorName;
-    $(this).attr('name', anchorName);
-    // Animating the move or scroll to the element is better, I think.
-    $('html,body').animate({scrollTop: $(this).offset().top},'slow');
+    el.attr('name', anchorName);
     return false;
   });
 
