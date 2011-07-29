a9ffd16a2e999014b72fee097c7cd9017b03f1d6
Miles Z. Sterrett <miles@mileszs.com>
2011-07-29 11:55:38 -0400

    First draft of "Why Indy?" page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ IndyhackersRails::Application.routes.draw do
   resources :job_post_requests, :only => [:new, :create]
   match 'job_post_request' => 'job_post_requests#new'
   match "blog", :to => "site#blog"
+  match "why_indy", :to => "site#why_indy"
   root :to => "site#index"
 end
