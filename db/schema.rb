fdb5fcd1cd2fc54a171cf2385cbe302633bbdc13
Miles Z. Sterrett <miles@mileszs.com>
2011-10-07 15:02:50 -0400

    Put "submit a job" in the menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110213163408) do
+ActiveRecord::Schema.define(:version => 20110728201151) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                             :default => "", :null => false
@@ -25,11 +25,79 @@ ActiveRecord::Schema.define(:version => 20110213163408) do
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
 
+  create_table "assignments", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "audit_id"
+    t.string   "url"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "ancestry"
+    t.datetime "due_at"
+    t.string   "page_type"
+    t.string   "state"
+  end
+
+  add_index "assignments", ["ancestry"], :name => "index_assignments_on_ancestry"
+  add_index "assignments", ["audit_id"], :name => "index_assignments_on_audit_id"
+  add_index "assignments", ["user_id"], :name => "index_assignments_on_user_id"
+
+  create_table "audits", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "generation_status"
+  end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
   create_table "jobs", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "questions", :force => true do |t|
+    t.string   "question"
+    t.string   "answer"
+    t.integer  "task_id"
+    t.integer  "assignment_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "role"
+    t.string   "first_name"
+    t.string   "last_name"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
