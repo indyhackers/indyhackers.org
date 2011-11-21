3a8ea2ae3612ffbd1936ed40653b2ff7174108d9
Miles Z. Sterrett <miles@mileszs.com>
2011-11-20 22:06:25 -0500

    It seriously took to now to figure that out?

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,6 +2,7 @@ class Admin::JobsController < InheritedResources::Base
   respond_to :html
 
   before_filter :authenticate_admin!
+  skip_before_filter :verify_authenticity_token, :only => :destroy
 
   def collection
     @jobs = Job.order("created_at DESC").all
