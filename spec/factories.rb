d398f36423c428f6f5b27c2abb4cf8895e2b84b0
Miles Z. Sterrett <miles@mileszs.com>
2012-04-14 14:19:49 -0400

    Migrate factories to new style

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,28 +1,30 @@
-Factory.sequence :email do |n|
-  "person#{n}@example.com"
-end
+FactoryGirl.define do
+  factory :job do
+    title 'Awesome Job'
+    description <<-MARKDOWN
+    This is an awesome job. You can tell this is an awesome job by the bullet points.
+      + Has awesome people
+      + Has awesome perks
+      + Has coffee
+      + Has awesome workplace (psst, it's your home)
+      + Has awesome moneys (we pay in yen)
+    MARKDOWN
+  end
 
-Factory.define :job do |j|
-  j.title 'Awesome Job'
-  j.description <<-MARKDOWN
-  This is an awesome job. You can tell this is an awesome job by the bullet points.
-    + Has awesome people
-    + Has awesome perks
-    + Has coffee
-    + Has awesome workplace (psst, it's your home)
-    + Has awesome moneys (we pay in yen)
-  MARKDOWN
-end
+  factory :admin do
+    sequence :email do |n|
+      "#{Faker::Internet.user_name}#{n}@example.org"
+    end
+  end
 
-Factory.define :admin do |a|
-  a.email { Factory.next :email }
-end
+  factory :viewer do
+    client_hash "asdfqwrogrwhoashogasdfgioha455"
+  end
 
-Factory.define :viewer do |v|
-  v.client_hash "asdfqwrogrwhoashogasdfgioha455"
-end
-
-Factory.define :user do |u|
-  u.email { Factory.next :email }
-  u.name 'Joe Camel'
+  factory :user do
+    sequence :email do |n|
+      "#{Faker::Internet.user_name}#{n}@example.org"
+    end
+    name "#{Faker::Name.name}"
+  end
 end
