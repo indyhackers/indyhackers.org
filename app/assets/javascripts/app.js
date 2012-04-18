a15e553d4ea8aaf67c32050b45d9a1e64e0b522a
Miles Z. Sterrett <miles@mileszs.com>
2012-04-17 23:34:30 -0400

    Load jobs on their own page
    No more weird javascript scrolling accordion thing.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,42 +1,7 @@
 $(document).ready(function() {
-  if (location.hash != '' && $('a[name=' + location.hash.replace(/#/, '') + ']').length) {
-    $('a[name=' + location.hash.replace(/#/, '') + ']').parent().next().slideDown('slow');
-    scrollTo($('a[name=' + location.hash.replace(/#/, '') + ']'));
+  if (location.hash != '' && $('section.jobs').length) {
+    window.location.replace('/jobs/' + location.hash.replace(/#/, ''));
   }
-  $('article.job h1 a').click(function(event) {
-    var el = $(this);
-    var anchorName = el.attr('name');
-    event.stopImmediatePropagation();
-    event.preventDefault();
-    if (el.parent().next().is(':hidden')) {
-      $.post(el.attr('href') + '/viewed');
-    }
-
-    el.parent().next().slideToggle('slow', function() {
-      if ($(this).is(':visible')) {
-        scrollTo(el);
-      }
-    });
-    // Remove the name so that setting the hash doesn't jump the page
-    el.attr('name', '');
-    location.hash = anchorName;
-    el.attr('name', anchorName);
-    return false;
-  });
-
-  $('.toggle-all a').click(function(event) {
-    event.stopImmediatePropagation();
-    event.preventDefault();
-
-    if ($(this).html().match(/collapse/)) {
-      $(this).html('+ expand all');
-      $('.job-description').slideUp('slow');
-    } else {
-      $(this).html('- collapse all');
-      $('.job-description').slideDown('slow');
-    }
-    return false;
-  });
 
   $('textarea#job_description, textarea#job_post_request_description').keyup(function(event) {
     var text = $(this).attr('value');
@@ -61,7 +26,3 @@ $(document).ready(function() {
     $('#preview article h1').text($(this).attr('value'));
   });
 });
-
-function scrollTo(element) {
-  $('html,body').animate({scrollTop: element.offset().top},'slow');
-}
