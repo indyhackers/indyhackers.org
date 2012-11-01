d1dd5b3583b01a9100dfd92cfb7c7bc8d6e5572d
Miles Z. Sterrett <miles@mileszs.com>
2012-11-01 08:10:02 -0400

    Latest schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,66 +16,27 @@ ActiveRecord::Schema.define(:version => 20120418031331) do
   create_table "admins", :force => true do |t|
     t.string   "email",                             :default => "", :null => false
     t.string   "encrypted_password", :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                     :default => "", :null => false
     t.integer  "failed_attempts",                   :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                        :null => false
+    t.datetime "updated_at",                                        :null => false
   end
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
 
-  create_table "assignments", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "audit_id"
-    t.string   "url"
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "ancestry"
-    t.datetime "due_at"
-    t.string   "page_type"
-    t.string   "state"
-  end
-
-  add_index "assignments", ["ancestry"], :name => "index_assignments_on_ancestry"
-  add_index "assignments", ["audit_id"], :name => "index_assignments_on_audit_id"
-  add_index "assignments", ["user_id"], :name => "index_assignments_on_user_id"
-
-  create_table "audits", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "generation_status"
-  end
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
   create_table "job_views", :force => true do |t|
     t.integer  "job_id"
     t.integer  "viewer_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "jobs", :force => true do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
     t.integer  "views"
     t.integer  "user_id"
     t.datetime "published_at"
@@ -91,17 +52,8 @@ ActiveRecord::Schema.define(:version => 20120418031331) do
     t.string   "title"
     t.datetime "published_at"
     t.string   "slug"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "questions", :force => true do |t|
-    t.string   "question"
-    t.string   "answer"
-    t.integer  "task_id"
-    t.integer  "assignment_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "slugs", :force => true do |t|
@@ -120,14 +72,14 @@ ActiveRecord::Schema.define(:version => 20120418031331) do
     t.string   "email"
     t.string   "token"
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "viewers", :force => true do |t|
     t.string   "client_hash"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
