b4c04276224fe9ed3fb8a9456635d01e55dab090
Miles Z. Sterrett <miles@mileszs.com>
2013-08-05 21:37:21 -0400

    Hide secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-IndyhackersRails::Application.config.secret_token = '4ef7f3c920e2aade871b21082bb5ad5fadf5e35bd9b0997f05c2705d2891f658c3f88565ca6d0232ea224c68a5a3d50c3d73089deed21b9eee49f06058fd6b71'
+if Rails.env.production?
+  IndyhackersRails::Application.config.secret_token = ENV['OMG_SECRET_TOKEN']
+else
+  IndyhackersRails::Application.config.secret_token = '4ef7f3c920e2aade871b21082bb5ad5fadf5e35bd9b0997f05c2705d2891f658c3f88565ca6d0232ea224c68a5a3d50c3d73089deed21b9eee49f06058fd6b71'
+end
