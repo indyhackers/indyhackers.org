b4771972300d1dddefdfeb7a2fe7af9a2acf5062
chris <netshade@gmail.com>
2016-12-01 00:40:39 -0500

    add validation for length constraint

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -2,7 +2,7 @@ require 'uri'
 
 class Redirect < ActiveRecord::Base
   validates :id,  format: /[a-z0-9_]+/i, presence: true
-  validates :url, presence: true
+  validates :url, presence: true, length: { maximum: 2083 }
   validate :url_is_valid
 
   def url_is_valid
