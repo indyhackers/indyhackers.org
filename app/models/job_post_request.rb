24f84b8254b36168ff2fc0b1429a4e3a5da7c16c
Miles Z. Sterrett <miles@mileszs.com>
2017-11-29 19:51:06 -0500

    Update the job board to use the new company field

--- app/models/job_post_request.rb
+++ app/models/job_post_request.rb
@@ -2,8 +2,8 @@ class JobPostRequest
   include ActiveModel::Validations
   include ActiveModel::Conversion
 
-  attr_accessor :id, :name, :email, :title, :description, :attachment
-  validates_presence_of :name, :email, :title, :description
+  attr_accessor :id, :name, :email, :title, :company, :description
+  validates_presence_of :name, :email, :title, :company, :description
 
   def initialize(attributes = {})
     attributes.each do |key, value|
