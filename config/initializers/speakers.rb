b0cf6b02d1ae7d53c44da003b0ddeb7178454c5c
Miles Z. Sterrett <miles@mileszs.com>
2020-06-03 09:12:21 -0400

    Add speakers list from former Diversify Indy list

---
+++ config/initializers/speakers.rb
@@ -0,0 +1,203 @@
+# frozen_string_literal: true
+
+SPEAKERS = [
+  {
+    name: 'Andrew Robinson III',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/arthree/',
+      email: 'mailto:ar3@ar3.me',
+    },
+    interests: ['Meetups'],
+    topics:
+      'Building product team habits leading to success, Product organizational design, from engineering to product to management, product and/or engineering at Lessonly',
+    biography:
+      'Andrew Robinson III serves the Product and Engineering department at Lessonly where he focuses on building a team that solves real problems for real people.',
+    pronouns: 'he/him/his',
+    picture: 'andrew-robinson.jpg',
+  },
+  {
+    name: 'Anna Eaglin',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/anna-eaglin/',
+      email: 'mailto:anna.eaglin@innovatemap.com',
+    },
+    interests: ['Meetups', 'Conferences'],
+    topics:
+      'Pride IN Tech, LGBTQ+ issues, LGBTQ+ issues in tech, women in tech, Product Management, Product Research, UX',
+    biography:
+      'Anna is a Principal Product Partner at Innovatemap where she leads the up the product research team and helps bring Indy’s biggest tech ideas to life. Whether she’s guiding customer research or conducting workshops, Anna helps emerging companies uncover their biggest challenges, prioritize their requirements, and ensure they build well-designed products. Anna is also the founder of Pride IN Tech, a networking group for LGBTQ+ professionals to make connections in the Indiana tech community.',
+    pronouns: 'she/her/hers',
+    picture: 'anna-eaglin.jpg',
+  },
+  {
+    name: 'Anna Saraceno',
+    contact: {
+      twitter: 'https://twitter.com/anna_saraceno',
+      website: 'https://annasaraceno.me/',
+      linkedin: 'https://www.linkedin.com/in/annasaraceno/',
+      email: 'mailto:annasaraceno@gmail.com',
+    },
+    interests: ['Meetups', 'Conferences', 'Companies'],
+    topics:
+      'Communicating the value of design within an organization. Self-assessment. Critique methods and sessions. Soft skills that are important to have as a designer. Finding and navigating a career path. I\'m open to anything that is of interest to someone who reads my profile.',
+    biography:
+      'Anna is a Senior Product Designer at Trello, which is part of the Atlassian ecosystem. Anna works remotely here in Indianapolis from her home that she shares with her wife (also named Anna), 2 sons, and a multitude of dogs and cats. Anna has held a number of positions as an in-house UX/Product Designer at multiple SaaS companies in Indianapolis, including Blackbaud, Salesforce, and Springbuk, with a short stint in freelancing.',
+    pronouns: 'she/her/hers',
+    picture: 'anna-saraceno.png',
+  },
+  {
+    name: 'Darye Henry',
+    contact: {
+      linkedin: 'https://linkedin.com/in/darye',
+      email: 'mailto:darye@afterschoolhq.com',
+    },
+    interests: ['Meetups', 'Conferences'],
+    topics:
+      'How to get software done and done right, minorities in tech, entrepreneurship, being a minority entrepreneur in tech, helping minorities succeed.',
+    biography:
+      'Darye is a Software Developer turned CTO, CEO, and Entrepreneur. Darye is currently the CTO at DT Starts here in Indianapolis and CEO of Afterschool HQ.',
+    pronouns: 'he/him/his',
+    picture: 'darye-henry.jpg',
+  },
+  {
+    name: 'Mya Williams',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/myaeliza/',
+      email: 'mailto:myaeliza15@gmail.com',
+    },
+    interests: ['Meetups', 'Conferences', 'Companies'],
+    topics:
+      'UX fundamentals, design thinking for non-designers, the importance of empathy in app creation, diversity & inclusion topics',
+    biography:
+      'Mya is a former middle school teacher who recently moved back to Indy from New Orleans. She has a Bachelors in Health Sciences with a concentration in Exercise Science. Mya came into tech through attending Kenzie Academy and she currently works as a UX Engineer at Crafted LLC. In her spare time, she enjoys attending local meetups and exploring the city with her dog, Georgie!',
+    pronouns: 'she/her/hers',
+    picture: 'mya-williams.jpg',
+  },
+  {
+    name: 'Caryssa Perez',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/caryssa-perez/',
+      website: 'https://www.caryssaperez.com',
+      email: 'mailto:caryssaperez@gmail.com',
+    },
+    interests: ['Meetups', 'Companies'],
+    topics:
+      'Diversity & Inclusion, product documentation, engineering enablement, UX basics, enabling non-product and non-engineering teams on technology',
+    biography:
+      'Caryssa is a CTO using her past experience as a software engineer to build a quality product for her company, Iris. She lives in Indianapolis with her partner, Irma; her two dogs, Lolita and Milo; and her cat, Genevieve.',
+    pronouns: 'she/her/hers',
+    picture: 'caryssa-perez.jpeg',
+  },
+  {
+    name: 'Melissa Crum',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/melissa-crum/',
+      email: 'mailto:melissa.crum17@gmail.com',
+    },
+    interests: ['Meetups', 'Conferences', 'Companies'],
+
+    topics: 'Analytics, Affiliate marketing, Email marketing',
+    biography:
+      'Melissa has 10 years of experience in Online Marketing with focus on affiliate marketing, paid marketing, and analytics. ',
+    pronouns: 'she/her/hers',
+    picture: 'melissa-crum.png',
+  },
+  {
+    name: 'Scott Soltys-Curry',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/scttcrry/',
+      website: 'https://indianapoliscoffeeguide.com',
+      instagram: 'https://instagram.com/heyscottcurry',
+      email: 'mailto:scttcrry@gmail.com',
+    },
+    interests: ['Meetups', 'Conferences', 'Companies'],
+    topics:
+      'UX Design, Diversity & Inclusion Tops, Self-Teaching Your Own Way Into Tech',
+    biography:
+      'Scott is the UX Lead at 120WaterAudit and is somewhat of a Scott of All Trades in the digital space -- including UX/UI design, product design, front-end development, business strategy, and marketing. (See what we mean?) He’s a Tennessee native transplanted in Indianapolis, where he lives with his husband and their two four-legged children.',
+    pronouns: 'he/him/his',
+    picture: 'scott-curry.jpg',
+  },
+  {
+    name: 'Eli Vida',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/elivida/',
+      email: 'mailto:evida@rightoninteractive.com',
+    },
+    interests: ['Meetups', 'Conferences', 'Companies'],
+    topics:
+      'Financial Literacy and Independance, Start-up Strategies, Bi-erasure, Diversity in team building, #honestcontent Social Media Content as a philosophy, Customer Narrative/customer lifecycle',
+    biography:
+      'Eli\'s passions lie in Philosophy, collaborative creativity, and honesty. Eli has been in the Social Media and Marketing space since highschool and he even worked for Chick-fil-a\'s Marketing Dept. (during the original scandal, btw). He also loves to brainstorm and help activate genius ideas.',
+    pronouns: 'he/him/his',
+    picture: 'eli-vida.jpg',
+  },
+  {
+    name: 'Tonie Snell',
+    contact: {
+      linkedin: 'www.linkedin.com/in/jobmingler',
+      email: 'mailto:tonie@360hr.co',
+    },
+    interests: ['Meetups', 'Conferences', 'Companies'],
+    topics: 'Workplace equality, Diversity Forward, Transformational Inclusion',
+    biography:
+      'Tonie relocated to Indianapolis three years ago. Her passion lies in moving diversity forward in talent acquisition, especially for those who stand at the intersection.',
+    pronouns: 'she/her/hers',
+    picture: 'toni-snell.jpg',
+  },
+  {
+    name: 'Ashlee Boyer',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/ashleemboyer/',
+      email: 'mailto:ashleemboyer.dev@gmail.com',
+    },
+    interests: ['Meetups', 'Conferences', 'Companies'],
+    topics:
+      'Accessibility from a design and technical perspective is what I\'m most interested in speaking about. I\'m also open to speaking about mental health or my experiences in tech as a hard of hearing person.',
+    biography:
+      'Ashlee just finished her Software Engineering degree and is working her second full-time job in software. Ashlee’s primary focus as a Front-End Web Developer is to make the internet a valuable experience for everyone. Accessibility isn\'t something to consider-it\'s absolutely necessary.',
+    pronouns: 'she/her/hers',
+    picture: 'ashlee-boyer.jpeg',
+  },
+  {
+    name: 'Linda Calvin',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/lindacalvin',
+      email: 'mailto:ashleemboyer.dev@gmail.com',
+    },
+    interests: ['Meetups', 'Conferences', 'Companies'],
+    topics: 'Diversity in tech, women in tech, demystifying what “tech” is',
+    biography:
+      'Linda has worn many hats in her 15+ year career. She started on a help desk, and since has held a number of positions in IT, serving as a digital IT leader for two Fortune 200 companies, an agile advocate, director of employee engagement at a local tech company. Linda has also worked in policy communications and as a civil prosecutor for the city of Indianapolis. She’s currently the VP for the School of IT at Ivy Tech Community College. In every role, she’s served as an advocate for diversity in the workforce.',
+    pronouns: 'she/her/hers',
+    picture: 'linda-calvin.png',
+  },
+  {
+    name: 'Melanie Malott',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/melaniemalott/',
+      email: 'mailto:melanie.malott@emarsys.com',
+    },
+    interests: ['Meetups', 'Conferences'],
+    topics:
+      'Diversity, Inclusion, Innovation, Global Organizations, Process Improvement',
+    biography:
+      'As a Cincinnati Native, Melanie moved to Indianapolis after graduating from Indiana University where she majored in Management with a Minor in Human Resources and a Business Foundations Certificate from the Kelley School of Business. She is passionate about diversity in the workplace and its ability to foster innovation in the workplace.',
+    pronouns: 'she/her/hers',
+    picture: 'melanie-malott.jpg',
+  },
+  {
+    name: 'Kate Maxwell',
+    contact: {
+      linkedin: 'https://www.linkedin.com/in/kate-maxwell-6a7a359/',
+      email: 'mailto:kate.maxwell@raytheon.com',
+    },
+    interests: ['Meetups', 'Conferences', 'Companies'],
+    topics:
+      'National Defense Strategy, fostering innovation and creativity in the workplace, corporate technology acquisition (particularly in national defense), STEM outreach, and Diversity & Inclusion.',
+    biography:
+      'Kate Maxwell is a tech executive in the Aerospace & Defense community, where she is the Engineering Lead for the Raytheon Indianapolis site and Chief Engineer of the Product Support business area. Kate has more than fifteen years of experience supporting programs for government customers, managing strategic R&D efforts across the business, and providing technical expertise in Mission Management and Unmanned Systems. Kate is a founding Director of the IIS Innovation Center, an organization which inspires and empowers Raytheon employees to innovate. She was named a 2019 Indianapolis Business Journal Woman of Influence and is a two-time national winner with the Society of Women Engineers.',
+    pronouns: 'she/her/hers',
+    picture: 'kate-maxwell.png',
+  },
+].freeze
