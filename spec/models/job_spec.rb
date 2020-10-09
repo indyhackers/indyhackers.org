e46a258d4863b8f003542ed5724f33f3b07429b7
Tony Drake <t27duck@gmail.com>
2020-10-09 19:31:25 -0400

    Cleanup existing specs

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Job do
   context "#active" do
     it "only includes jobs that are published and active" do
-      @unpublished_job = create(:job, :unpublished)
-      @future_published_job = create(:job, :published_in_future)
-      @published_job = create(:job, :published)
-      @no_longer_active_job = create(:job, :no_longer_active)
+      unpublished_job = create(:job, :unpublished)
+      future_published_job = create(:job, :published_in_future)
+      published_job = create(:job, :published)
+      no_longer_active_job = create(:job, :no_longer_active)
 
       results = Job.active
 
-      expect(results).to include(@published_job)
-      expect(results).not_to include(@unpublished_job)
-      expect(results).not_to include(@future_published_job)
-      expect(results).not_to include(@no_longer_active_job)
+      expect(results).to include(published_job)
+      expect(results).not_to include(unpublished_job)
+      expect(results).not_to include(future_published_job)
+      expect(results).not_to include(no_longer_active_job)
     end
   end
 end
--- spec/models/viewer_spec.rb
+++ spec/models/viewer_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Viewer do
   it "should return true if the viewer has viewed the given job" do
-    @job = FactoryBot.create(:job)
-    @viewer = FactoryBot.create(:viewer)
-    @viewer.jobs << @job
-    expect(@viewer.viewed?(@job)).to eq true
+    job = create(:job)
+    viewer = create(:viewer)
+    viewer.jobs << job
+    expect(viewer.viewed?(job)).to eq true
   end
 
   it "should return false if the viewer has not viewed the given job" do
-    @job = FactoryBot.create(:job)
-    @viewer = FactoryBot.create(:viewer)
-    expect(@viewer.viewed?(@job)).to eq false
+    job = create(:job)
+    viewer = create(:viewer)
+    expect(viewer.viewed?(job)).to eq false
   end
 end
