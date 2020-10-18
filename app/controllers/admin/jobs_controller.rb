fdb927c960e0f4f930504547f6c3de1cc418de47
Tony Drake <t27duck@gmail.com>
2020-10-18 16:12:35 -0400

    Remove inherited resources for a standard Rails controller

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,31 +1,50 @@
-class Admin::JobsController < InheritedResources::Base
-  respond_to :html
-
+class Admin::JobsController < ApplicationController
   before_action :authenticate_admin!
+  before_action :fetch_job, only: [:show, :edit, :update, :destroy]
   skip_before_action :verify_authenticity_token, :only => :destroy
+
+  def index
+    @jobs = Job.order(created_at: :desc)
+  end
+
+  def show
+  end
 
   def new
     @job = Job.new
   end
 
+  def edit
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to admin_job_path(@job), :notice => 'Job was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def update
-    @job = resource
     if @job.update(job_params)
-      redirect_to @job, :notice => 'Job was updated successfully'
+      redirect_to job_path(@job), :notice => 'Job was updated successfully'
     else
       render :edit
     end
   end
 
-  def collection
-    @jobs = Job.order("created_at DESC").all
-  end
-
-  def resource
-    @job = Job.find_using_slug(params[:id])
+  def destroy
+    @job.destroy
+    redirect_to admin_jobs_path, notice: 'Job was successfully destroyed.'
   end
 
   private
+
+  def fetch_job
+    @job = Job.find_using_slug(params[:id])
+  end
 
   def job_params
     params.require(:job).permit(:title, :description, :company, :published_at)
