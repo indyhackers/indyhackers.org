ae9f4c4009e77a2ac59fa902e0e2f43898e30622
Aaron Milam <aaron@milam.io>
2021-04-20 21:11:33 -0400

    Show salary on job board list

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,12 +1,12 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_admin!
-  before_action :fetch_job, only: [:show, :edit, :update, :destroy]
-  skip_before_action :verify_authenticity_token, :only => :destroy
+  before_action :fetch_job, only: %i[show edit update destroy]
+  skip_before_action :verify_authenticity_token, only: :destroy
 
   def index
     @jobs = Job.order(created_at: :desc)
 
-    params[:job_view]  ||= 'all'
+    params[:job_view] ||= 'all'
     case params[:job_view]
     when 'active'
       @jobs = @jobs.active
@@ -19,21 +19,19 @@ class Admin::JobsController < ApplicationController
     @jobs = @jobs.page(params[:page])
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @job = Job.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @job = Job.new(job_params)
 
     if @job.save
-      redirect_to admin_job_path(@job), :notice => 'Job was successfully created.'
+      redirect_to admin_job_path(@job), notice: 'Job was successfully created.'
     else
       render :new
     end
@@ -41,7 +39,7 @@ class Admin::JobsController < ApplicationController
 
   def update
     if @job.update(job_params)
-      redirect_to job_path(@job), :notice => 'Job was updated successfully'
+      redirect_to job_path(@job), notice: 'Job was updated successfully'
     else
       render :edit
     end
@@ -59,6 +57,6 @@ class Admin::JobsController < ApplicationController
   end
 
   def job_params
-    params.require(:job).permit(:title, :description, :company, :published_at)
+    params.require(:job).permit(:title, :description, :company, :salary, :published_at)
   end
 end
