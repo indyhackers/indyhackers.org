70717d5c5a7c55164af2bcfd6e348a642d87cfc5
Aaron Milam <aaron@milam.io>
2021-04-20 20:58:06 -0400

    Add salary to jobs and provide a way to add one when posting a job

--- app/models/job_post_request.rb
+++ app/models/job_post_request.rb
@@ -2,12 +2,13 @@ class JobPostRequest
   include ActiveModel::Validations
   include ActiveModel::Conversion
 
-  attr_accessor :id, :name, :email, :title, :company, :description
+  attr_accessor :id, :name, :email, :title, :company, :salary, :description
+
   validates_presence_of :name, :email, :title, :company, :description
 
   def initialize(attributes = {})
     attributes.each do |key, value|
-      self.send("#{key}=", value)
+      send("#{key}=", value)
     end
   end
 
