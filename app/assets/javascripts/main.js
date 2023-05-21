87889fa84aa9e5479b2dbb6b954f0401c3258244
Tony Drake <t27duck@gmail.com>
2023-05-21 10:40:41 -0400

    Remove unused JavaScript
    The amount of JavaScript cruft is too damn high.
    
    - Remove jquery.blockUI - The api `$.blockUI()` is not used anywhere in the app.
    - Remove linkify - The api `linkify()` is not used anywhere in the app.
    - Remvoe jquery.elastic - All textareas already have a width of 100% so they will adjust based on the browser window.
    - Remove jquery.easing - Nothing is using a custom easing function nor is the jquery defualt changed anywhere.
    - Remove jquery.qtip and styles - It's not loaded up in application.js nor are the CSS classes used anywhere.
    - Remove jquery.supersized - It's not loaded up in application.js nor are the CSS classes used anywhere.
    - Remove jquery.carouFredSel - It's not loaded up in application.js.
    - Remove script.js - No other JS references these functions.
    - Remove jquery.fancybox - There's nothing fancy going on here and it's not even loaded.
    - Remove jquery.ui - Nope, not used.
    - Remove jquery.min - ... we're using the jquery-rails gem already...
    - Moved JavaScript in the app layout from the body to the head. This isn't 2010 anymore.
    - Refactored most of the `main.js` code to not rely on jQuery.
    
    From what I can see, all the Holiday Social archive pages are each in their own self-contained folder under `public` and vendor their own JavaScript files (which probably also aren't used 90% of the time), but I left those alone.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,27 +1,56 @@
 $(document).ready(function() {
-  $('textarea').elastic();
   $('form.new_job_post_request, form.new_job, form.edit_job, #sign_in form').superLabels({
     labelLeft: 10,
     labelTop: 10
   });
+});
 
-  $('textarea#job_description, textarea#job_post_request_description').keyup(function(event) {
-    var text = $(this).val();
-    var converter = new Showdown.converter();
-    var html = converter.makeHtml(text);
-    $('#markdown-target').html(html);
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Job title preview
+  document.querySelectorAll('input.job_title, input.job_company').forEach(function(elm) {
+      elm.addEventListener('keyup', function(event) {
+      var name = document.querySelector('input.job_title').value + " at " + document.querySelector('input.job_company').value;
+      document.querySelector('#single-job.job.readable h1').textContent = name;
+    });
   });
 
-  $('input.job_title, input.job_company').keyup(function(event) {
-    var name = $('input.job_title').val() + " at " + $('input.job_company').val();
-    $('#single-job.job.readable h1').text(name);
+  // Markdown preview
+  document.querySelectorAll('textarea#job_description, textarea#job_post_request_description').forEach(function(elm) {
+    elm.addEventListener('keyup', function(event) {
+      var text = event.target.value;
+      var converter = new Showdown.converter();
+      var html = converter.makeHtml(text);
+      document.querySelector('#markdown-target').innerHTML = html;
+    });
   });
 
-  $('.tab-container').not('.active').hide();
-  $("ul.tab-menu li").click(function (ev) {
-    ev.preventDefault();
-    $('ul.tab-menu li').removeClass('active');
-    $(this).addClass('active');
-    $('.tab-container').hide().eq($(this).index()).show();
+  // Tab logic
+  document.querySelectorAll('.tab-container').forEach(function(elm) {
+    if (!elm.classList.contains('active')) {
+      elm.style.display = 'none';
+    }
+  });
+  document.querySelectorAll('ul.tab-menu li').forEach(function(tabElm, tabIndex) {
+    tabElm.addEventListener('click', function(event) {
+      event.preventDefault();
+      document.querySelectorAll('ul.tab-menu li').forEach(function(tabElm2) {
+        if (tabElm === tabElm2) {
+          tabElm2.classList.add('active');
+        } else {
+          tabElm2.classList.remove('active');
+        }
+      });
+
+      document.querySelectorAll('.tab-container').forEach(function(containerElm, containerIndex) {
+        if (tabIndex === containerIndex) {
+          containerElm.classList.add('active');
+          containerElm.style.display = 'block';
+        } else {
+          containerElm.classList.remove('active');
+          containerElm.style.display = 'none';
+        }
+      });
+    });
   });
 });
