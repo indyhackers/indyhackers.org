9fc459b94c8529336597444f2f838c90c2abbc0b
Andrew Poland <andrew@poland.cx>
2024-05-13 13:42:30 -0400

    add recaptcha to job post request form

--- app/controllers/job_post_requests_controller.rb
+++ app/controllers/job_post_requests_controller.rb
@@ -18,7 +18,9 @@ class JobPostRequestsController < ApplicationController
       SystemMailer.job_post_request(@job_post_request, @job).deliver_now
       redirect_to new_job_post_request_path, notice: "Your job post request was sent successfully!"
     else
-      flash[:notice] = "There was a problem with sending your job post request.  Verify all fields are present and the reCAPTCHA is checked."
+      flash[:notice] =
+        "There was a problem with sending your job post request.  "\
+        "Verify all fields are present and the reCAPTCHA is checked."
       render :new
     end
   end
