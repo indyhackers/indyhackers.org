b82db242264a8fe73db4f4ad275adee300202971
Andrew Poland <andrew@poland.cx>
2025-03-14 13:43:04 -0400

    commit WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2023_06_16_164549) do
+ActiveRecord::Schema[8.0].define(version: 2024_09_06_210216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
